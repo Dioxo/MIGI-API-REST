bf55c1c684728142caba754ad5411faf566b6102
Dioxo <amolinares19@gmail.com>
2020-01-23 22:11:28 +0100

    added tags related to notes when getNotes called

--- src/routes/notes/store.js
+++ src/routes/notes/store.js
@@ -4,13 +4,49 @@ function getNotes(idUser) {
     return new Promise((resolve, reject) => {
         const sql = 'SELECT id_note,title,description,have_revision FROM note WHERE id_user = ?';
 
-        connection.query(sql, [idUser], (err, result) =>{
+        connection.query(sql, [idUser], async (err, result) =>{
             if (err)
                 reject (err);
             //return result data
+
+
+            for (let i = 0 ; i < result.length ; i++){
+                result[i].tags = [];
+                //try to insert each tag to the note they belong to
+                try{
+                    let data = await getTags(idUser, result[i].id_note );
+
+                    let tags = [];
+                    data.forEach( row  =>{
+                        tags.push({
+                            text_tag : row.text_tag,
+                            color_tag : row.color_tag
+                        });
+                    });
+
+                    result[i].tags = tags;
+                }catch (e) {
+                    reject(e);
+                }
+            }
+
             resolve(result);
         })
     })
+}
+//
+function getTags(idUser, idNote){
+    return new Promise((resolve, reject) => {
+
+        const sql = 'select text_tag, color_tag from note_tag natural join tag where id_user = ? and id_note = ?  GROUP BY text_tag';
+        connection.query(sql, [idUser, idNote], (err, result ) => {
+            if (err)
+                reject(err);
+
+            resolve(result);
+        });
+
+    })  ;
 }
 
 module.exports = {
--- test/note.test.js
+++ test/note.test.js
@@ -21,6 +21,13 @@ describe('Note API TEST ', () => {
         expect(note).to.have.property('id_note');
         expect(note).to.have.property('title');
         expect(note).to.have.property('description');
+        expect(note).to.have.property('tags');
+        expect(note.tags).to.be.an.instanceOf(Array);
+
+        console.log(note);
+        const tags = note.tags[0];
+        expect(tags).to.have.property('text_tag');
+        expect(tags).to.have.property('color_tag');
     });
 
     it('GET /api/notes when no id specified' , async() => {
