a92b8c39aca5b073fcd6e26ad2d89aab44c24f79
Dioxo <amolinares19@gmail.com>
2020-01-23 20:01:04 +0100

    let createConnection when necessary, not call it directly

--- src/routes/tag/store.js
+++ src/routes/tag/store.js
@@ -1,8 +1,6 @@
 const debug =require('debug')('server:debug');
 const connection = require('../../db_connect');
 
-connection.connect();
-
 function getTags(idUser) {
     return new Promise((resolve, reject) => {
         const sql = 'SELECT * FROM tag where id_user = ?';
