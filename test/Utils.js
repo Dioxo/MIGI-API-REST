738a14a5c3585aa5b4400194e788aefdb5c35a6b
Dioxo <amolinares19@gmail.com>
2020-01-24 16:35:26 +0100

    create class Utils to use in test

---
+++ test/Utils.js
@@ -0,0 +1,22 @@
+class Utils {
+    constructor() {
+    }
+    /**
+     *
+     * @param length   - Size of the string to generate randomly
+     * @returns {string}
+     */
+    static makeid(length) {
+        var result           = '';
+        var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        var charactersLength = characters.length;
+        for ( var i = 0; i < length; i++ ) {
+            result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
+        return result;
+    }
+}
+
+
+
+module.exports = Utils;
